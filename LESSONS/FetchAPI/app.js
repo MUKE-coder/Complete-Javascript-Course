23fba660e61c9ccf001c9f126f6a63ee36c29082
MUKE-coder <gmukejohnbaptist@gmail.com>
2022-10-31 07:53:49 +0300

    added spinner

--- LESSONS/FetchAPI/app.js
+++ LESSONS/FetchAPI/app.js
@@ -1,4 +1,4 @@
-// fetch("https://jsonplaceholder.typicode.com/posts")
+const errorMessage = document.querySelector(".error");
 //   .then((response) => response.json())
 //   .then((data) => {
 //     const users = data;
@@ -8,10 +8,17 @@
 const productContainer = document.querySelector(".products");
 console.log(productContainer);
 async function getProducts() {
-  const response = await fetch("https://fakestoreapi.com/products");
-
-  console.log(response);
-  if (response.status === 200) {
+  try {
+    renderSpinner(productContainer);
+    const response = await fetch("https://fakestoreapi.com/products");
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error(
+          `The url (${response.url} is unavailable, please check again) (${response.status})`
+        );
+      }
+    }
+    console.log(response);
     const data = await response.json();
     console.log(data);
     let htmlOutput = "";
@@ -29,7 +36,25 @@ async function getProducts() {
       </div>
       `;
     });
-    document.querySelector(".products").innerHTML = htmlOutput;
+    clearMarkup(productContainer);
+    productContainer.insertAdjacentHTML("afterbegin", htmlOutput);
+  } catch (error) {
+    showError(productContainer, error);
+    console.log(error);
   }
 }
 getProducts();
+function renderSpinner(parentEl) {
+  const markup =
+    '<div class="spinner"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" /> </svg></div>';
+  clearMarkup(parentEl);
+  parentEl.insertAdjacentHTML("afterbegin", markup);
+}
+function clearMarkup(parentEl) {
+  parentEl.innerHTML = "";
+}
+function showError(parentEl, msg) {
+  const markup = `<h1 class="error">${msg}</h1>`;
+  clearMarkup(parentEl);
+  parentEl.insertAdjacentHTML("afterbegin", markup);
+}
